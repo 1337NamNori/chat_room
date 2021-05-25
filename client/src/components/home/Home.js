02c48e64d9ca4be4fceccd4d31498b470a0dc4eb
1337NamNori <nam20051999@gmail.com>
2021-05-25 15:06:56 +0700

    Remove user helper

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -14,8 +14,8 @@ export default function Home() {
         return () => {
             socket.emit('disconnect');
             socket.off();
-        }
-    }, [ENDPOINT])
+        };
+    }, [ENDPOINT]);
 
     const { user, setUser } = useContext(UserContext);
     const rooms = [
@@ -27,38 +27,40 @@ export default function Home() {
             name: 'room2',
             _id: '2',
         },
-    ]
-    const handleSubmit = e => {
+    ];
+    const handleSubmit = (e) => {
         e.preventDefault();
         socket.emit('create-room', room);
         console.log(room);
         setRoom('');
-    }
+    };
     const setAsJohn = () => {
         const john = {
             name: 'John',
             email: 'john@email.com',
             password: '123',
             id: '123',
-        }
+        };
         setUser(john);
-    }
+    };
     const setAsTom = () => {
         const tom = {
             name: 'Tom',
             email: 'tom@email.com',
             password: '456',
             id: '456',
-        }
+        };
         setUser(tom);
-    }
+    };
     return (
         <div className="container">
             <div className="row">
                 <div className="col s12 m6">
                     <div className="card blue-grey darken-1">
                         <div className="card-content white-text">
-                            <span className="card-title">Welcome {user ? user.name : ''}</span>
+                            <span className="card-title">
+                                Welcome {user ? user.name : ''}
+                            </span>
                             <div className="row">
                                 <form
                                     className="col s12"
@@ -72,9 +74,13 @@ export default function Home() {
                                                 type="text"
                                                 className="validate"
                                                 value={room}
-                                                onChange={e => setRoom(e.target.value)}
+                                                onChange={(e) =>
+                                                    setRoom(e.target.value)
+                                                }
                                             />
-                                            <label htmlFor="room-name">Room</label>
+                                            <label htmlFor="room-name">
+                                                Room
+                                            </label>
                                         </div>
                                     </div>
                                     <button className="btn">Create Room</button>
@@ -82,8 +88,12 @@ export default function Home() {
                             </div>
                         </div>
                         <div className="card-action">
-                            <a href="#" onClick={setAsJohn}>Set as John</a>
-                            <a href="#" onClick={setAsTom}>Set as Tom</a>
+                            <a href="#" onClick={setAsJohn}>
+                                Set as John
+                            </a>
+                            <a href="#" onClick={setAsTom}>
+                                Set as Tom
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -92,10 +102,9 @@ export default function Home() {
                 </div>
             </div>
 
-
             <Link to={'/chat'}>
-                < button className="btn"> Go to chat</button>
+                <button className="btn"> Go to chat</button>
             </Link>
-        </div >
-    )
+        </div>
+    );
 }
--- server/helpers.js
+++ server/helpers.js
@@ -1,7 +1,9 @@
 let users = [];
 
 const addUser = (socketID, roomID, userID, username) => {
-    const exist = users.some(user => user.userID === userID && user.roomID === roomID);
+    const exist = users.some(
+        (user) => user.userID === userID && user.roomID === roomID,
+    );
     if (exist) {
         return { error: 'User already exists in this room' };
     } else {
@@ -10,16 +12,16 @@ const addUser = (socketID, roomID, userID, username) => {
         console.log(users);
         return { user };
     }
-}
+};
 
 const removeUser = (socketID) => {
-    const index = users.findIndex(user => user.socketID === socketID)
+    const index = users.findIndex((user) => user.socketID === socketID);
     if (index !== -1) {
         return users.splice(index, 1)[0];
     }
-}
+};
 
 module.exports = {
     addUser,
-    removeUser
-}
+    removeUser,
+};
